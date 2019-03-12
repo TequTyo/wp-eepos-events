e0c9ca44dcd240c58fd8eb6e488e89a038b95b5d
Miikka Virtanen <dragory.up@gmail.com>
2019-03-12 13:20:07 +0200

    Fix foreign key in install

--- install.php
+++ install.php
@@ -10,11 +10,18 @@ function eepos_events_install() {
 			`event_id` INT(10) UNSIGNED NOT NULL,
 			`post_id` BIGINT(10) UNSIGNED NOT NULL,
 			PRIMARY KEY (`event_id`),
-			INDEX `fk_posts` (`post_id`),
-			CONSTRAINT `fk_posts` FOREIGN KEY (`post_id`) REFERENCES `wp_posts` (`ID`) ON UPDATE CASCADE ON DELETE CASCADE
+			INDEX `fk_posts` (`post_id`)
 		) COLLATE='utf8mb4_swedish_ci'
     ";
 
 	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 	dbDelta( $logTableSql );
+
+	$wpdb->query("
+		ALTER TABLE {$wpdb->eepos_events->log}
+		ADD CONSTRAINT `fk_posts`
+		  FOREIGN KEY (`post_id`) REFERENCES `wp_posts` (`ID`)
+		    ON UPDATE CASCADE
+		    ON DELETE CASCADE
+	");
 }
