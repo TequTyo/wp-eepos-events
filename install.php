4e30a6d60691210fd89104fa54c41d2ebb68a25f
Miikka Virtanen <2606411+Dragory@users.noreply.github.com>
2019-11-28 14:18:11 +0200

    Support multisite/network installs

--- install.php
+++ install.php
@@ -1,26 +1,50 @@
 <?php
 
-function eepos_events_install() {
+function eepos_events_install_current_site() {
 	global $wpdb;
 
 	eepos_events_init();
 
-	$logTableSql = "
-		CREATE TABLE {$wpdb->eepos_events_log} (
-			`event_id` INT(10) UNSIGNED NOT NULL,
-			`post_id` BIGINT(10) UNSIGNED NOT NULL,
-			PRIMARY KEY (`event_id`)
-		) COLLATE='utf8mb4_swedish_ci'
-    ";
+	$LATEST_DB_VERSION = 1;
+	$dbVersion         = intval(get_option( "eepos_events_db_version", 0 ));
 
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-	dbDelta( $logTableSql );
+	if ( $dbVersion < 1 ) {
+		$logTableSql = "
+			CREATE TABLE {$wpdb->eepos_events_log} (
+				`event_id` INT(10) UNSIGNED NOT NULL,
+				`post_id` BIGINT(10) UNSIGNED NOT NULL,
+				PRIMARY KEY (`event_id`)
+			) COLLATE='utf8mb4_swedish_ci'
+	    ";
 
-	$wpdb->query("
-		ALTER TABLE {$wpdb->eepos_events_log}
-		ADD CONSTRAINT `fk_posts`
-			FOREIGN KEY (`post_id`) REFERENCES `wp_posts` (`ID`)
-				ON UPDATE CASCADE
-				ON DELETE CASCADE
-	");
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $logTableSql );
+
+		$wpdb->query( "
+			ALTER TABLE {$wpdb->eepos_events_log}
+			ADD CONSTRAINT `fk_eepos_events_log_posts`
+				FOREIGN KEY (`post_id`) REFERENCES `wp_posts` (`ID`)
+					ON UPDATE CASCADE
+					ON DELETE CASCADE
+		" );
+	}
+
+	update_option( "eepos_events_db_version", $LATEST_DB_VERSION, 'yes' );
+}
+
+function eepos_events_install( $network_wide ) {
+	if ( $network_wide ) {
+		$site_ids = get_sites( [ 'fields' => 'ids', 'network_id' => get_current_network_id() ] );
+		foreach ( $site_ids as $site_id ) {
+			// Setup on other site
+			switch_to_blog( $site_id );
+			eepos_events_install_current_site();
+
+			// Restore current site
+			restore_current_blog();
+			eepos_events_init();
+		}
+	} else {
+		eepos_events_install_current_site();
+	}
 }
