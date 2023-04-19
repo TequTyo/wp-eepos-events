d91fa3246f5ab410bd2a72d9e36954c5431886aa
Teemu <99242709+TequTyo@users.noreply.github.com>
2023-04-19 12:02:45 +0300

    Display dynamic tags date & location with default value automatically for current post

--- dynamic-tags/eepos_variable_dynamic_tag_date.php
+++ dynamic-tags/eepos_variable_dynamic_tag_date.php
@@ -21,73 +21,41 @@ class Elementor_Dynamic_Tag_Eepos_Variable_Tag_2 extends \Elementor\Core\Dynamic
         return [ \Elementor\Modules\DynamicTags\Module::TEXT_CATEGORY ];
     }
 
-    protected function get_eepos_posts() {
-        global $wpdb;
-
-        $query = $wpdb->prepare("SELECT {$wpdb->posts}.ID FROM {$wpdb->posts}
-            INNER JOIN {$wpdb->postmeta} AS startDateMeta ON startDateMeta.post_id = {$wpdb->posts}.id AND startDateMeta.meta_key = 'event_start_date'
-            INNER JOIN {$wpdb->postmeta} AS startTimeMeta ON startTimeMeta.post_id = {$wpdb->posts}.id AND startTimeMeta.meta_key = 'event_start_time'
-            LEFT JOIN {$wpdb->term_relationships} ON {$wpdb->term_relationships}.object_id = {$wpdb->posts}.id
-            LEFT JOIN {$wpdb->term_taxonomy} ON {$wpdb->term_taxonomy}.term_taxonomy_id = {$wpdb->term_relationships}.term_taxonomy_id
-            LEFT JOIN {$wpdb->terms} ON {$wpdb->terms}.term_id = {$wpdb->term_taxonomy}.term_id
-            WHERE {$wpdb->posts}.post_type = 'eepos_event'
-            AND {$wpdb->posts}.post_status = 'publish'
-            AND startDateMeta.meta_value >= CURDATE() GROUP BY {$wpdb->posts}.ID
-            ORDER BY startDateMeta.meta_value ASC, startTimeMeta.meta_value ASC");
-        $posts = $wpdb->get_results( $query );
-        return array_map( function ( $p ) {
-            return $p->ID;
-        }, $posts);
-    }
-
     protected function register_controls() {
-        // hae nykyisen postauksen perusteella aika ja paikka tiedot
-        $variables_date = [];
+        $post = get_post( get_the_ID() );
+        $post->meta = get_post_meta( $post->ID );
 
-        $listEventIds = $this->get_eepos_posts();
-        $posts = count( $listEventIds )
-               ? get_posts( [
-                   'include'   => $listEventIds,
-                   'post_type' => 'eepos_event'
-               ] )
-               : [];
-        foreach ( $posts as $post ) {
-            $post->meta = get_post_meta( $post->ID );
+        $startDate = $post->meta['event_start_date'][0];
+        $startTime = $post->meta['event_start_time'][0];
+        $currentYear = date( 'Y' );
 
-            $startDate = $post->meta['event_start_date'][0];
-            $startTime = $post->meta['event_start_time'][0];
-            $currentYear = date( 'Y' );
-
-            if ( $startDate ) {
-                $startCombined = $startDate . ' ' . $startTime;
-                $startDateDT = new DateTime( $startCombined );
-                if ( $startDateDT->format( 'Y' ) === $currentYear ) {
-                    $startDateFormatted = $startDateDT->format( 'd.n. \k\l\o G.i' );
-                } else {
-                    $startDateFormatted = $startDateDT->format( 'd.n.Y \k\l\o G.i' );
-                }
-            }
-            if (!empty($startDateFormatted)) {
-                $variables_date[ $startDateFormatted ] = ucwords( str_replace( '_', ' ', $startDateFormatted ) );
+        if ( $startDate ) {
+            $startCombined = $startDate . ' ' . $startTime;
+            $startDateDT = new DateTime( $startCombined );
+            if ( $startDateDT->format( 'Y' ) === $currentYear ) {
+                $startDateFormatted = $startDateDT->format( 'd.n. \k\l\o G.i' );
+            } else {
+                $startDateFormatted = $startDateDT->format( 'd.n.Y \k\l\o G.i' );
             }
         }
+        if (!empty($startDateFormatted)) {
+            $startDateFormatted = ucwords( str_replace( '_', ' ', $startDateFormatted ) );
+        }
+
         $this->add_control(
             'user_selected_variables_date',
             [
-                'type' => \Elementor\Controls_Manager::SELECT,
+                'type' => \Elementor\Controls_Manager::TEXT,
                 'label' => esc_html__( 'Date', 'textdomain' ),
-                'options' => $variables_date,
+                'default' => $startDateFormatted,
             ]
         );
     }
 
     public function render() {
-        $user_selected_variable_date = $this->get_settings( 'user_selected_variables_date' );
+        $user_variable_date = $this->get_settings_for_display();
 
-        if ( ! $user_selected_variable_date && ! isset( $_SERVER[ $user_selected_variable_date ] )) {
-            return;
-        }
-
-        echo wp_kses_post( $user_selected_variable_date );
+        echo '<p>' . $user_variable_date['user_selected_variables_date']. '</p>';
     }
+
 }
--- dynamic-tags/eepos_variable_dynamic_tag_location.php
+++ dynamic-tags/eepos_variable_dynamic_tag_location.php
@@ -21,60 +21,27 @@ class Elementor_Dynamic_Tag_Eepos_Variable_Tag_1 extends \Elementor\Core\Dynamic
         return [ \Elementor\Modules\DynamicTags\Module::TEXT_CATEGORY ];
     }
 
-    protected function get_eepos_posts() {
-        global $wpdb;
-
-        $query = $wpdb->prepare("SELECT {$wpdb->posts}.ID FROM {$wpdb->posts}
-            INNER JOIN {$wpdb->postmeta} AS startDateMeta ON startDateMeta.post_id = {$wpdb->posts}.id AND startDateMeta.meta_key = 'event_start_date'
-            INNER JOIN {$wpdb->postmeta} AS startTimeMeta ON startTimeMeta.post_id = {$wpdb->posts}.id AND startTimeMeta.meta_key = 'event_start_time'
-            LEFT JOIN {$wpdb->term_relationships} ON {$wpdb->term_relationships}.object_id = {$wpdb->posts}.id
-            LEFT JOIN {$wpdb->term_taxonomy} ON {$wpdb->term_taxonomy}.term_taxonomy_id = {$wpdb->term_relationships}.term_taxonomy_id
-            LEFT JOIN {$wpdb->terms} ON {$wpdb->terms}.term_id = {$wpdb->term_taxonomy}.term_id
-            WHERE {$wpdb->posts}.post_type = 'eepos_event'
-            AND {$wpdb->posts}.post_status = 'publish'
-            AND startDateMeta.meta_value >= CURDATE() GROUP BY {$wpdb->posts}.ID
-            ORDER BY startDateMeta.meta_value ASC, startTimeMeta.meta_value ASC");
-        $posts = $wpdb->get_results( $query );
-        return array_map( function ( $p ) {
-            return $p->ID;
-        }, $posts);
-    }
-
     protected function register_controls() {
-        $variables_location = [];
-
-        $listEventIds = $this->get_eepos_posts();
-        $posts = count( $listEventIds )
-               ? get_posts( [
-                   'include'   => $listEventIds,
-                   'post_type' => 'eepos_event'
-               ] )
-               : [];
-        foreach ( $posts as $post ) {
-            $post->meta = get_post_meta( $post->ID );
-            $get_loc = $post->meta['location'][0];
-            if (!empty($get_loc)) {
-                $variables_location[ $get_loc ] = ucwords( str_replace( '_', ' ', $get_loc ) );
-            }
+        $post = get_post( get_the_ID() );
+        $post->meta = get_post_meta( $post->ID );
+        $get_loc = $post->meta['location'][0];
+        if (!empty($get_loc)) {
+            $get_loc  = ucwords( str_replace( '_', ' ', $get_loc ) );
         }
-        natcasesort($variables_location);
+
         $this->add_control(
-            'user_selected_variables',
+            'user_selected_variables_location',
             [
-                'type' => \Elementor\Controls_Manager::SELECT,
+                'type' => \Elementor\Controls_Manager::TEXT,
                 'label' => esc_html__( 'Location', 'textdomain' ),
-                'options' => $variables_location,
+                'default' => $get_loc,
             ]
         );
     }
 
     public function render() {
-        $user_selected_variable = $this->get_settings( 'user_selected_variables' );
+        $user_selected_variable = $this->get_settings_for_display();
 
-        if ( ! $user_selected_variable && ! isset( $_SERVER[ $user_selected_variable ] )) {
-            return;
-        }
-
-        echo wp_kses_post( $user_selected_variable );
+        echo '<p>' . $user_selected_variable['user_selected_variables_location']. '</p>';
     }
 }
